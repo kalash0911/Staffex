e947089286d7c0db10b8942b93ef4cef6f0ba999
Yaroslav Zhuravlev <zhuravlev.yaroslav@gmail.com>
2023-12-28 16:57:17 +0200

    audio test

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,90 +1,92 @@
 /* For burger */
 
-const burger = document.querySelector(".burger");
-const menuBody = document.querySelector(".menu-wrap");
-const linkClose = document.querySelectorAll(".link-close");
-const overflow = document.querySelector(".overflow");
+const burger = document.querySelector('.burger');
+const menuBody = document.querySelector('.menu-wrap');
+const linkClose = document.querySelectorAll('.link-close');
+const overflow = document.querySelector('.overflow');
 
 if (burger) {
-    burger.addEventListener("click", function (e) {
-        document.body.classList.toggle("body_lock");
-        document.body.classList.toggle("active");
-        if (burger.classList.contains('burger_active')) {
-            burger.classList.add('burger_finish');
-            burger.classList.remove('burger_active');
-            overflow.classList.toggle("overflow_active");
-        } else {
-            burger.classList.add('burger_active');
-            burger.classList.remove('burger_finish');
-            overflow.classList.toggle("overflow_active");
-        }
-    });
-};
+  burger.addEventListener('click', function (e) {
+    document.body.classList.toggle('body_lock');
+    document.body.classList.toggle('active');
+    if (burger.classList.contains('burger_active')) {
+      burger.classList.add('burger_finish');
+      burger.classList.remove('burger_active');
+      overflow.classList.toggle('overflow_active');
+    } else {
+      burger.classList.add('burger_active');
+      burger.classList.remove('burger_finish');
+      overflow.classList.toggle('overflow_active');
+    }
+  });
+}
 
 if (overflow) {
-    overflow.addEventListener("click", function (e) {
-        document.body.classList.toggle("body_lock");
-        document.body.classList.toggle("active");
-        if (burger.classList.contains('burger_active')) {
-            burger.classList.add('burger_finish');
-            burger.classList.remove('burger_active');
-            overflow.classList.toggle("overflow_active");
-        } else {
-            burger.classList.add('burger_active');
-            burger.classList.remove('burger_finish');
-            overflow.classList.toggle("overflow_active");
-        }
-    });
-};
+  overflow.addEventListener('click', function (e) {
+    document.body.classList.toggle('body_lock');
+    document.body.classList.toggle('active');
+    if (burger.classList.contains('burger_active')) {
+      burger.classList.add('burger_finish');
+      burger.classList.remove('burger_active');
+      overflow.classList.toggle('overflow_active');
+    } else {
+      burger.classList.add('burger_active');
+      burger.classList.remove('burger_finish');
+      overflow.classList.toggle('overflow_active');
+    }
+  });
+}
 
 if (linkClose.length) {
-    for (var i = 0; i < linkClose.length; ++i) {
-        linkClose[i].addEventListener("click", function (e) {
-            document.body.classList.remove("body_lock");
-            document.body.classList.remove("active");
-            burger.classList.remove("burger_active");
-            burger.classList.add('burger_finish');
-            menuBody.classList.remove("menu_active");
-            overflow.classList.remove("overflow_active");
-        })
-    }
-};
-
+  for (var i = 0; i < linkClose.length; ++i) {
+    linkClose[i].addEventListener('click', function (e) {
+      document.body.classList.remove('body_lock');
+      document.body.classList.remove('active');
+      burger.classList.remove('burger_active');
+      burger.classList.add('burger_finish');
+      menuBody.classList.remove('menu_active');
+      overflow.classList.remove('overflow_active');
+    });
+  }
+}
 
 /* For Page full scroll */
 
 var fullpageInstance;
 
 function toggleFullPage() {
-    var screenSize = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  var screenSize =
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth;
 
-    if (screenSize <= 767) {
-        if (fullpageInstance) {
-            fullpageInstance.destroy('all');
-            fullpageInstance = null;
-        }
-    } else {
-        if (!fullpageInstance) {
-            // @ts-ignore
-            fullpageInstance = new fullpage("#fullpage", {
-                scrollingSpeed: 1000,
-            });
-        }
+  if (screenSize <= 767) {
+    if (fullpageInstance) {
+      fullpageInstance.destroy('all');
+      fullpageInstance = null;
     }
+  } else {
+    if (!fullpageInstance) {
+      // @ts-ignore
+      fullpageInstance = new fullpage('#fullpage', {
+        scrollingSpeed: 1000,
+      });
+    }
+  }
 }
 
 toggleFullPage();
 
 window.addEventListener('resize', function () {
-    toggleFullPage();
+  toggleFullPage();
 });
 
 function onEntry(entry) {
-    entry.forEach(change => {
-        if (change.isIntersecting) {
-            change.target.classList.add('show');
-        }
-    });
+  entry.forEach((change) => {
+    if (change.isIntersecting) {
+      change.target.classList.add('show');
+    }
+  });
 }
 
 /* Scroll anim */
@@ -93,87 +95,84 @@ let options = { threshold: [0.5] };
 let observer = new IntersectionObserver(onEntry, options);
 let elements = document.querySelectorAll('.anim');
 for (let elm of elements) {
-    observer.observe(elm);
+  observer.observe(elm);
 }
 
 /* Text effect */
 
 document.addEventListener('DOMContentLoaded', function () {
-    const shuffleElements = document.querySelectorAll('.shuffle');
-    const speed = 50;
+  const shuffleElements = document.querySelectorAll('.shuffle');
+  const speed = 50;
 
-    function updateText(element, originalText, iteration) {
-        let shuffledText = '';
-        const textLength = originalText.length;
+  function updateText(element, originalText, iteration) {
+    let shuffledText = '';
+    const textLength = originalText.length;
 
-        for (let i = 0; i < textLength; i++) {
-            const randomIndex = Math.floor(Math.random() * textLength);
-            shuffledText += originalText.charAt(randomIndex);
-        }
-
-        element.textContent = shuffledText;
-
-
-        if (shuffledText !== originalText && iteration < 20) {
-            setTimeout(() => updateText(element, originalText, iteration + 1), speed);
-        } else {
-            setTimeout(() => {
-                element.textContent = originalText;
-            }, speed);
-        }
+    for (let i = 0; i < textLength; i++) {
+      const randomIndex = Math.floor(Math.random() * textLength);
+      shuffledText += originalText.charAt(randomIndex);
     }
 
-    function handleShowClass(element) {
-        const originalText = element.textContent;
+    element.textContent = shuffledText;
 
-        if (element.classList.contains('show')) {
-            updateText(element, originalText, 0);
-        }
+    if (shuffledText !== originalText && iteration < 20) {
+      setTimeout(() => updateText(element, originalText, iteration + 1), speed);
+    } else {
+      setTimeout(() => {
+        element.textContent = originalText;
+      }, speed);
     }
+  }
 
-    shuffleElements.forEach(function (shuffleElement) {
-        handleShowClass(shuffleElement);
+  function handleShowClass(element) {
+    const originalText = element.textContent;
 
-        const observer = new MutationObserver(() => {
-            handleShowClass(shuffleElement);
-        });
+    if (element.classList.contains('show')) {
+      updateText(element, originalText, 0);
+    }
+  }
 
-        observer.observe(shuffleElement, { attributes: true });
+  shuffleElements.forEach(function (shuffleElement) {
+    handleShowClass(shuffleElement);
+
+    const observer = new MutationObserver(() => {
+      handleShowClass(shuffleElement);
     });
+
+    observer.observe(shuffleElement, { attributes: true });
+  });
 });
 
 /* Mouse move effect */
 
-
-document.addEventListener('mousemove', e => {
-    Object.assign(document.documentElement, {
-        style: `
-		--move-x: ${(e.clientX - window.innerWidth / 2) * -.003}deg;
-		--move-y: ${(e.clientY - window.innerHeight / 2) * .005}deg;
-		`
-    })
-})
-
-/* Song Btn */
-
-document.addEventListener("DOMContentLoaded", function () {
-    const songBtn: HTMLElement | null = document.querySelector('.song-btn');
-    const audio: HTMLAudioElement | null = document.querySelector('.song-btn audio');
-
-    if (songBtn && audio) {
-        audio.play()
-        audio.muted = false
-        songBtn.addEventListener('click', function () {
-
-            if (audio.muted === undefined) {
-                audio.muted = true;
-            } else {
-                audio.muted = !audio.muted;
-            }
-
-            songBtn.classList.toggle('mute');
-        });
-    }
+document.addEventListener('mousemove', (e) => {
+  Object.assign(document.documentElement, {
+    style: `
+		--move-x: ${(e.clientX - window.innerWidth / 2) * -0.003}deg;
+		--move-y: ${(e.clientY - window.innerHeight / 2) * 0.005}deg;
+		`,
+  });
 });
 
+/* Song Btn */
+document.addEventListener('DOMContentLoaded', function () {
+  const songBtn: HTMLElement | null = document.querySelector('.song-btn');
+  const audio: HTMLAudioElement | null =
+    document.querySelector('.song-btn audio');
 
+  if (songBtn && audio) {
+    audio.removeAttribute('muted');
+    audio.muted = false;
+    songBtn.addEventListener('click', function () {
+      if (!audio.muted) {
+        audio.setAttribute('muted', 'false');
+        audio.muted = true;
+      } else {
+        audio.removeAttribute('muted');
+        audio.muted = false;
+      }
+
+      songBtn.classList.toggle('mute');
+    });
+  }
+});
