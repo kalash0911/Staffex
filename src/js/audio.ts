7f16d264bef0c5f6af96fc557d323362acbd2bc0
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2024-01-09 19:15:08 +0200

    changed volume and fix slider

--- src/js/audio.ts
+++ src/js/audio.ts
@@ -1,12 +1,13 @@
 export const initAudioClicks = () => {
-  function playSound() {
-    var audio = new Audio('./files/click-song.mp3');
-    audio.play();
-  }
+    function playSound() {
+        var audio = new Audio('./files/click-song.mp3');
+        audio.play();
+        audio.volume = 0.1;
+    }
 
-  var clickButtons = document.querySelectorAll('.click-song');
+    var clickButtons = document.querySelectorAll('.click-song');
 
-  clickButtons.forEach(function (button) {
-    button.addEventListener('click', playSound);
-  });
+    clickButtons.forEach(function (button) {
+        button.addEventListener('click', playSound);
+    });
 };
