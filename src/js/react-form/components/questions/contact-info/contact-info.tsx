40f86fa2e66cdc2b186270e9d88a22dcc27840f6
Yaroslav Zhuravlev <zhuravlev.yaroslav@gmail.com>
2024-01-14 23:35:25 +0200

    Update contact-info.tsx

--- src/js/react-form/components/questions/contact-info/contact-info.tsx
+++ src/js/react-form/components/questions/contact-info/contact-info.tsx
@@ -117,7 +117,6 @@ export const ContactInfo = () => {
                     </>
                 ) : (
                     <>
-                        {' '}
                         <TextField
                             {...register('firstName')}
                             label="First name"
