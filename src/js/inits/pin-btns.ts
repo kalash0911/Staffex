928fce2d96458d5164520c8a9ae10f7209e58e42
Yaroslav Zhuravlev <zhuravlev.yaroslav@gmail.com>
2024-01-14 15:32:43 +0200

    pin all mob btns

--- src/js/inits/pin-btns.ts
+++ src/js/inits/pin-btns.ts
@@ -3,92 +3,95 @@ export const pinButtonsOnScroll = () => {
         const screenSize = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
         if (screenSize >= 600) return;
 
-        let oldScroll = 0;
-        let isUpDirection = false;
+        const sectionSelectors = ['.secretary', '.smm-manager', '.accountant'];
 
-        const secretarySection = document.querySelector('.secretary');
-        if (!secretarySection) return;
-        const botBlock = secretarySection.querySelector('.bot-wrap');
-        const secretaryPinBtn = secretarySection.querySelector('#secretaryPinBtn');
-        const endBtnPlaceholder = document.querySelector('.pinBtnEnd') as HTMLDivElement;
-        endBtnPlaceholder.style.height = `${secretaryPinBtn.getBoundingClientRect().height}px`;
+        sectionSelectors.forEach((selector) => {
+            let oldScroll = 0;
+            let isUpDirection = false;
+            const section = document.querySelector(selector);
+            if (!section) return;
+            const botBlock = section.querySelector('.bot-wrap');
+            const sectionPinBtn = section.querySelector('.pinBtn');
+            const endBtnPlaceholder = section.querySelector('.pinBtnEnd') as HTMLDivElement;
+            endBtnPlaceholder.style.height = `${sectionPinBtn.getBoundingClientRect().height}px`;
 
-        if (!secretaryPinBtn) return;
+            if (!sectionPinBtn) return;
 
-        const onEntry: IntersectionObserverCallback = (entry) => {
-            entry.forEach((change) => {
-                if (change.isIntersecting) {
-                    change.target.classList.add('inViewport');
-                } else {
-                    change.target.classList.remove('inViewport');
-                }
-
-                if (change.isIntersecting && !isUpDirection) {
-                    secretaryPinBtn.classList.remove('d-none');
-                    secretaryPinBtn.classList.add('fixed');
-                    secretaryPinBtn.classList.add('transition');
-                    setTimeout(() => {
-                        secretaryPinBtn.classList.add('slideUp');
-                    }, 50);
-                    setTimeout(() => {
-                        secretaryPinBtn.classList.remove('transition');
-                    }, 300);
-                }
-            });
-        };
-
-        /* Scroll anim */
-
-        let options = { threshold: [0.8] };
-        let observer = new IntersectionObserver(onEntry, options);
-        observer.observe(botBlock);
-
-        const pinBtnEndObs = new IntersectionObserver(
-            (entry) => {
-                entry.forEach((btn) => {
-                    if (btn.isIntersecting) {
-                        secretaryPinBtn.classList.remove('fixed');
-                        secretaryPinBtn.classList.remove('slideUp');
-                        (btn.target as HTMLDivElement).style.height = `0px`;
+            const onEntry: IntersectionObserverCallback = (entry) => {
+                entry.forEach((change) => {
+                    if (change.isIntersecting) {
+                        change.target.classList.add('inViewport');
                     } else {
-                        endBtnPlaceholder.style.height = `${secretaryPinBtn.getBoundingClientRect().height}px`;
+                        change.target.classList.remove('inViewport');
+                    }
+
+                    if (change.isIntersecting && !isUpDirection) {
+                        sectionPinBtn.classList.remove('d-none');
+                        sectionPinBtn.classList.add('fixed');
+                        sectionPinBtn.classList.add('transition');
+                        setTimeout(() => {
+                            sectionPinBtn.classList.add('slideUp');
+                        }, 50);
+                        setTimeout(() => {
+                            sectionPinBtn.classList.remove('transition');
+                        }, 300);
                     }
                 });
-            },
-            { threshold: 0 },
-        );
-        pinBtnEndObs.observe(endBtnPlaceholder);
+            };
 
-        window.addEventListener('scroll', (event) => {
-            isUpDirection = oldScroll > scrollY;
-            oldScroll = scrollY;
-            const windowHeight = window.innerHeight;
-            const btnBottom = secretaryPinBtn.getBoundingClientRect().bottom;
-            const botBlockBottom = botBlock.getBoundingClientRect().bottom;
+            /* Scroll anim */
 
-            if (!isUpDirection) {
-                return;
-            }
+            let options = { threshold: [0.8] };
+            let observer = new IntersectionObserver(onEntry, options);
+            observer.observe(botBlock);
 
-            if (
-                !secretaryPinBtn.classList.contains('d-none') &&
-                !secretaryPinBtn.classList.contains('slideDown') &&
-                btnBottom >= windowHeight
-            ) {
-                secretaryPinBtn.classList.add('fixed');
-                secretaryPinBtn.classList.add('slideUp');
-                // setTimeout(() => {
-                //     secretaryPinBtn.classList.add('transition');
-                // }, 350);
-            }
+            const pinBtnEndObs = new IntersectionObserver(
+                (entry) => {
+                    entry.forEach((btn) => {
+                        if (btn.isIntersecting) {
+                            sectionPinBtn.classList.remove('fixed');
+                            sectionPinBtn.classList.remove('slideUp');
+                            (btn.target as HTMLDivElement).style.height = `0px`;
+                        } else {
+                            endBtnPlaceholder.style.height = `${sectionPinBtn.getBoundingClientRect().height}px`;
+                        }
+                    });
+                },
+                { threshold: 0 },
+            );
+            pinBtnEndObs.observe(endBtnPlaceholder);
 
-            if (!secretaryPinBtn.classList.contains('d-none') && botBlockBottom - 85 >= windowHeight) {
-                secretaryPinBtn.classList.add('transition');
-                secretaryPinBtn.classList.remove('slideUp');
-                setTimeout(() => {
-                    secretaryPinBtn.classList.add('d-none');
-                }, 150);
-            }
+            window.addEventListener('scroll', (event) => {
+                isUpDirection = oldScroll > scrollY;
+                oldScroll = scrollY;
+                const windowHeight = window.innerHeight;
+                const btnBottom = sectionPinBtn.getBoundingClientRect().bottom;
+                const botBlockBottom = botBlock.getBoundingClientRect().bottom;
+
+                if (!isUpDirection) {
+                    return;
+                }
+
+                if (
+                    !sectionPinBtn.classList.contains('d-none') &&
+                    !sectionPinBtn.classList.contains('slideDown') &&
+                    btnBottom >= windowHeight
+                ) {
+                    sectionPinBtn.classList.add('fixed');
+                    sectionPinBtn.classList.add('slideUp');
+                    // setTimeout(() => {
+                    //     secretaryPinBtn.classList.add('transition');
+                    // }, 350);
+                }
+
+                if (!sectionPinBtn.classList.contains('d-none') && botBlockBottom - 85 >= windowHeight) {
+                    sectionPinBtn.classList.add('transition');
+                    sectionPinBtn.classList.remove('slideUp');
+                    setTimeout(() => {
+                        sectionPinBtn.classList.add('d-none');
+                    }, 150);
+                }
+            });
         });
     };
 
