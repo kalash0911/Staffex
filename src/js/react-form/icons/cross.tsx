8f2f7083bb44e5d1c609a66ba78f1656ef6e6a8f
Yaroslav Zhuravlev <zhuravlev.yaroslav@gmail.com>
2024-01-17 01:37:58 +0200

    fix warnings

---
+++ src/js/react-form/icons/cross.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const CrossIcon = () => {
+    return (
+        <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clipPath="url(#clip0_1471_1371)">
+                <rect x="17.1016" y="0.161133" width="1" height="24" transform="rotate(45 17.1016 0.161133)" />
+                <rect x="0.132812" y="0.868164" width="1" height="10.5" transform="rotate(-45 0.132812 0.868164)" />
+                <rect x="17.8086" y="17.1309" width="1" height="10.5" transform="rotate(135 17.8086 17.1309)" />
+            </g>
+            <defs>
+                <clipPath id="clip0_1471_1371">
+                    <rect width="18" height="18" fill="white" />
+                </clipPath>
+            </defs>
+        </svg>
+    );
+};
--- src/js/react-form/icons/plus.tsx
+++ src/js/react-form/icons/plus.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const IconPlus = () => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
-            <g clip-path="url(#clip0_1012_6216)">
+            <g clipPath="url(#clip0_1012_6216)">
                 <path
                     d="M9.66758 1.18139L8.33424 1.18139L8.33438 8.33347L1.18229 8.33333V9.66667L8.33424 9.66667L8.33424 16.8186L9.66758 16.8186L9.66757 9.66667L16.8195 9.66667V8.33333L9.66757 8.33333L9.66758 1.18139Z"
                     fill="#5CFF4F"
