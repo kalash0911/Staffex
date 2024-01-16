93fd37aabff8f5e9ffa2f291e36c308ca44b7c14
Yaroslav Zhuravlev <zhuravlev.yaroslav@gmail.com>
2024-01-17 01:51:45 +0200

    Update validation.ts

--- src/js/react-form/components/questions/contact-info/validation.ts
+++ src/js/react-form/components/questions/contact-info/validation.ts
@@ -2,11 +2,14 @@ import * as yup from 'yup';
 import { EMAIL_INVALID, PHONE_INVALID, REQUIRED_FIELD } from '../../../constants/err-msgs';
 import { EMAIL_REGEX, PHONE_REGEX } from '../../../constants/regex';
 
-export const schema = yup
-    .object({
+export const schema = yup.object().shape(
+    {
         isB2B: yup.boolean(),
         phone: yup.string().required(REQUIRED_FIELD).matches(PHONE_REGEX, { message: PHONE_INVALID }),
-        altPhone: yup.string().matches(PHONE_REGEX, { message: PHONE_INVALID }),
+        altPhone: yup.string().when('altPhone', {
+            is: (value: string) => value?.length,
+            then: (schema) => schema.matches(PHONE_REGEX, { message: PHONE_INVALID }),
+        }),
         email: yup.string().required(REQUIRED_FIELD).matches(EMAIL_REGEX, { message: EMAIL_INVALID }),
         companyName: yup.string().when('isB2B', {
             is: true,
@@ -32,5 +35,6 @@ export const schema = yup
             is: false,
             then: (schema) => schema.required(REQUIRED_FIELD),
         }),
-    })
-    .required();
+    },
+    [['altPhone', 'altPhone']],
+);
