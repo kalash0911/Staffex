f4c3ecc28ab13bcdcba234313299dea1f78db8ba
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2024-01-17 22:04:29 +0200

    added styles for skip pop-up

--- src/js/react-form/components/modals/skip/skip-modal.tsx
+++ src/js/react-form/components/modals/skip/skip-modal.tsx
@@ -9,15 +9,12 @@ export interface ISkipModalProps extends IModalBasicProps {
 
 export const SkipModal = ({ onClose, onSkip }: ISkipModalProps) => {
     return (
-        <div>
-            <h2>Are you sure you want to skip this step?</h2>
-            <p>
-                If you skip this step, mentioned features will not be available. Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit.
-            </p>
-            <div className="btn-wrap">
-                <Button label="Cancel" onClick={onClose} />
-                <Button label="Skip" onClick={onSkip} />
+        <div className="modal-content">
+            <h2 className="modal-title">Are you sure you want to skip this step?</h2>
+            <p className="modal-text">If you skip this step, mentioned features will not be available.</p>
+            <div className="modal-btn-wrap">
+                <Button label="Cancel" onClick={onClose} variant="secondary" />
+                <Button label="Skip anyway" onClick={onSkip} />
             </div>
         </div>
     );
--- src/js/react-form/components/shared/modal/modal.tsx
+++ src/js/react-form/components/shared/modal/modal.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { IModalBasicProps } from '../../../models/common/modal';
+import { CrossIcon } from '../../../icons/cross';
 
 export const Modal = ({ open, onClose, children, size = 'md' }: IModalBasicProps) => {
     return (
         open && (
             <div className={`modal-wrap ${size}`}>
-                <button onClick={onClose}>X</button>
+                <button className="btn-close" onClick={onClose}>
+                    <CrossIcon />
+                </button>
                 {children}
                 <div className="backdrop" onClick={onClose}></div>
             </div>
