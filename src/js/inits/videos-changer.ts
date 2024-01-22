739da97ec4cc937ba2fa6fd88487b1ad104349af
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2024-01-22 20:04:33 +0200

    added styles for LIST OF ADDED

--- src/js/inits/videos-changer.ts
+++ src/js/inits/videos-changer.ts
@@ -1,22 +1,6 @@
 import { supportsHEVCAlpha } from '../utils/general';
 
 export const initVideoChanges = () => {
-    const playerBots = document.querySelectorAll('.bot-video') as NodeListOf<HTMLVideoElement>;
-
-    if (playerBots.length) {
-        playerBots.forEach((video, ind) => {
-            video.src = supportsHEVCAlpha() ? `./files/bot${ind + 1}.mov` : `./files/bot${ind + 1}.webm`;
-        });
-    }
-
-    const playerBotsLoop = document.querySelectorAll('.bot-loop-video') as NodeListOf<HTMLVideoElement>;
-
-    if (playerBotsLoop.length) {
-        playerBotsLoop.forEach((video, ind) => {
-            video.src = supportsHEVCAlpha() ? `./files/bot-loop${ind + 1}.mov` : `./files/bot-loop${ind + 1}.webm`;
-        });
-    }
-
     const playerSpeak = document.querySelectorAll('.speak-video') as NodeListOf<HTMLVideoElement>;
 
     if (playerSpeak.length) {
