1b00b9206de8dc77199a3b5c53cad7ef9ac09ecf
Yaroslav Zhuravlev <zhuravlev.yaroslav@gmail.com>
2024-01-26 14:37:57 +0200

    Update arrow.tsx

--- src/js/react-form/icons/arrow.tsx
+++ src/js/react-form/icons/arrow.tsx
@@ -4,8 +4,8 @@ export const Arrow = () => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
             <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 d="M9.99952 15.2078L0.689453 5.89782L1.79431 4.79297L9.99952 12.9981L18.2047 4.79297L19.3095 5.89782L9.99952 15.2078Z"
                 fill="white"
             />
