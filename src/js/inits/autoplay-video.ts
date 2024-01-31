4d660ff60c8e2f7473beb6ef8fd6cb1c7470a406
Yaroslav Zhuravlev <zhuravlev.yaroslav@gmail.com>
2024-01-31 13:05:48 +0200

    NodeListOf .video-play

--- src/js/inits/autoplay-video.ts
+++ src/js/inits/autoplay-video.ts
@@ -1,41 +1,40 @@
 export interface ScrollHandler {
-  (event: Event): void;
+    (event: Event): void;
 }
 
 export const initAutoPlayVideoOnScroll = () => {
-  function isElementInViewport(el: HTMLElement): boolean {
-    const rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
-
-  function handleScroll(): void {
-    const videoElement = document.querySelector(
-      '.video-play',
-    ) as HTMLVideoElement;
-
-    if (videoElement && isElementInViewport(videoElement)) {
-      if (videoElement.paused) {
-        videoElement
-          .play()
-          .then(() => {
-            setTimeout(() => {
-              videoElement.pause();
-            }, videoElement.duration * 1000);
-          })
-          .catch((error) => {
-            console.error('Error', error);
-          });
-      }
-      window.removeEventListener('scroll', scrollHandler);
+    function isElementInViewport(el: HTMLElement): boolean {
+        const rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
     }
-  }
 
-  const scrollHandler: ScrollHandler = handleScroll;
-  window.addEventListener('scroll', scrollHandler);
+    function handleScroll(): void {
+        const videoElements = document.querySelectorAll('.video-play') as NodeListOf<HTMLVideoElement>;
+
+        videoElements.forEach((videoElement) => {
+            if (videoElement && isElementInViewport(videoElement)) {
+                if (videoElement.paused) {
+                    videoElement
+                        .play()
+                        .then(() => {
+                            setTimeout(() => {
+                                videoElement.pause();
+                            }, videoElement.duration * 1000);
+                        })
+                        .catch((error) => {
+                            console.error('Error', error);
+                        });
+                }
+                window.removeEventListener('scroll', scrollHandler);
+            }
+        });
+    }
+
+    const scrollHandler: ScrollHandler = handleScroll;
+    window.addEventListener('scroll', scrollHandler);
 };
