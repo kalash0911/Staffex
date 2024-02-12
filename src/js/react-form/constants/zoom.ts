420c63164aae40c64c7084c7eadc629cebc2f33d
Yaroslav Zhuravlev <zhuravlev.yaroslav@gmail.com>
2024-02-12 14:21:46 +0200

    test ZOOM_REDIRECT_URL

--- src/js/react-form/constants/zoom.ts
+++ src/js/react-form/constants/zoom.ts
@@ -2,4 +2,5 @@ import { getHost } from '../utils/general';
 
 export const ZOOM_API_KEY = 'BOskjakMSp2yQh6WVGz7KQ';
 export const ZOOM_REDIRECT_URL = `${getHost()}/success-zoom-auth.html`;
+console.log('ZOOM_REDIRECT_URL: ', ZOOM_REDIRECT_URL);
 export const ZOOM_AUTH_URL = `https://zoom.us/oauth/authorize?response_type=code&client_id=${ZOOM_API_KEY}&redirect_uri=${ZOOM_REDIRECT_URL}`;
