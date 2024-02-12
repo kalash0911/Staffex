dbffef44df241c79b9bf9ca8a68bac8a193da4f2
VBatiuchenko <roket98@gmail.com>
2024-02-12 13:36:44 +0200

    changed to new

--- src/js/react-form/constants/urls.ts
+++ src/js/react-form/constants/urls.ts
@@ -1,6 +1,6 @@
 // APIs:
 // TODO: change to deployed url:
-export const STAFFEX_BASE_URL = '8c26-77-120-140-128.ngrok-free.app';
+export const STAFFEX_BASE_URL = '0b4a-77-120-140-128.ngrok-free.app';
 export const STAFFEX_API = `https://${STAFFEX_BASE_URL}`;
 export const STAFFEX_WEB_SOCKET_URL = `wss://${STAFFEX_BASE_URL}/ws/leantech/entity`;
 export const STAFFEX_GOOGLE_AUTH = `${STAFFEX_API}/auth/google`;
