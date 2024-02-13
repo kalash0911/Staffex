fa451f242a45f14eb28ac5193e8989f2637a5632
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2024-02-13 22:06:06 +0200

    added smm manager page

--- src/js/inits/videos-changer.ts
+++ src/js/inits/videos-changer.ts
@@ -9,13 +9,21 @@ export const initVideoChanges = () => {
         });
     }
 
-    const playerBotsEl = document.querySelectorAll('.bot-video-el') as NodeListOf<HTMLVideoElement>;
+    const playerBotsEl = document.querySelectorAll('.bvel1') as NodeListOf<HTMLVideoElement>;
 
     if (playerBotsEl.length) {
         playerBotsEl.forEach((video, ind) => {
             video.src = supportsHEVCAlpha()
                 ? `./files/secretary/secretary${ind + 1}.mov`
                 : `./files/secretary/secretary${ind + 1}.webm`;
+        });
+    }
+
+    const playerBotsEl2 = document.querySelectorAll('.bvel2') as NodeListOf<HTMLVideoElement>;
+
+    if (playerBotsEl2.length) {
+        playerBotsEl2.forEach((video, ind) => {
+            video.src = supportsHEVCAlpha() ? `./files/manager/manager${ind + 1}.mov` : `./files/manager/manager${ind + 1}.webm`;
         });
     }
 };
